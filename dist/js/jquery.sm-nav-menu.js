4d4da38304ab47b607c63c911bc8b4edd78a9b34
Sujan Molla <smitexpert@gmail.com>
2019-12-15 16:43:28 +0600

    Price Costing Running

--- dist/js/jquery.sm-nav-menu.js
+++ dist/js/jquery.sm-nav-menu.js
@@ -1,9 +1,11 @@
 (function($){
     $.fn.smNavMenu = function(){
+        var rightMenu = $(".sm-nav-right-menu ul").html();
         $(this).addClass('sm-responsive-nav');
-        $(".sm-responsive-nav ul").prepend('<li class="toggle-menu"><a href="#">Menu</a> <button id="sm-nav-collaps"><i class="fas fa-bars"></i></li>');
+        $(".sm-responsive-nav ul").prepend('<li class="toggle-menu"><a href="#">Menu</a><ul class="left-collaps">'+rightMenu+'</ul><button id="sm-nav-collaps"><i class="fas fa-bars"></i></button></li>');
         $("#sm-nav-collaps").click(function(){
             $(".sm-responsive-nav ul li a").toggleClass("toggle");
         })
     }
 })(jQuery)
+
--- dist/scripts.js
+++ dist/scripts.js
@@ -1,3 +1,14 @@
 // $(".sm-responsive-nav").find('ul').append('<li id="sm-nav-collaps"><i class="fas fa-bars"></i></li>');
 
 $("#header_nav").smNavMenu();
+
+$("#tarcking").click(function(){
+    $(".dml-banner-search label").addClass('label');
+})
+
+$("#tarcking").focusout(function(){
+    var data = $(this).val();
+    if(data == ""){
+        $(".dml-banner-search label").removeClass('label');
+    }
+})
